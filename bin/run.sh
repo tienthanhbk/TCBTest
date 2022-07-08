e23688d895619cc9d0f93789f991013fbe7f0185
sayohyeah <thanhbuitien@admicro.vn>
2022-07-08 14:16:12 +0700

    Fix run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 BASE_DIR=$(dirname $(readlink -e $0))/..
 
-SERVER_CONFIG=$BASE_DIR/config/server.properties
 LOG4J_CONFIG=$BASE_DIR/config/log4j.xml
 LOG_PATH=$BASE_DIR/logs
 LOG_LEVEL=info
-JAVA_HEAP_MAX=-Xmx2024m
+JAVA_HEAP_MAX=-Xmx1024m
 
-MAIN_JAR=target/empty-project-1.0.jar
-LIB=lib
-CLASS_PATH=$JAVA_HOME/lib/*:$LIB/*:$MAIN_JAR
+MAIN_JAR=target/pool-project-1.0.jar
 
 echo $BASE_DIR
 echo $LOG4J_CONFIG
@@ -17,8 +14,7 @@ echo $SERVER_CONFIG
 
 while (true)
 do
-java -Dapp.conf=$SERVER_CONFIG \
-     -Dlog4j.configuration=$LOG4J_CONFIG \
+java -Dlog4j.configuration=$LOG4J_CONFIG \
      -Dlogging.path=$LOG_PATH \
      -Dlogging.level.root=$LOG_LEVEL \
      $JAVA_HEAP_MAX \
